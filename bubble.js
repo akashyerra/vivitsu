817365f442818ef30a1e955db08c7222178696d3
kvenkatamar <78538730+kvenkatamar@users.noreply.github.com>
2023-04-18 19:29:06 +0530

    VIVITSU --version 0.18

--- bubble.js
+++ bubble.js
@@ -1,18 +1,12 @@
 let bubbles=document.getElementsByClassName("bubble");
 let curs=document.querySelector(".cursor");
-console.log(curs.style);
-
-console.log(bubbles);
-
-function p(){
-    console.log("working")
-
-}
+const audio = document.querySelector('#bubble-sound');
 
 for(let i=0; i<bubbles.length; i++){
     bubbles[i].addEventListener("click",()=>{
         console.log("clicked");
         bubbles[i].style.display="none";
+        audio.play();
         setTimeout(()=>{
             bubbles[i].style.display="flex";
         }, 1000);
@@ -24,9 +18,9 @@ let animationBlock=document.getElementById("background-wrap")
 var myScrollFunc = function() {
     var y = window.scrollY;
     if (y >= 100) {
-      animationBlock.style.display = "none"
+      animationBlock.style.display = "none";
     } else {
-      animationBlock.style.display = "block"
+      animationBlock.style.display = "block";
     }
   };
   
